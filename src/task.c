a36316b4eb9f4867c85fed7686066dbef4e44dea
cjkied <i.punk@qq.com>
2020-05-30 21:09:24 +0800

    Correct crystal frequency to 11.0592Mhz.

--- src/task.c
+++ src/task.c
@@ -5,8 +5,8 @@
 #include "app_stopwatch.h"
 #include "task.h"
 
-#define ISR_T0_INTERVAL 918 //918-1ms,1836-2ms,4589-5ms,9179-10ms,45893-50ms
-#define ISR_T1_INTERVAL 45902 //0.99998911s as simulation result.  //918-1ms,1836-2ms,4589-5ms,9179-10ms,45893-50ms
+#define ISR_T0_INTERVAL 922 //922-1ms,1843-2ms,4608-5ms,9216-10ms,46080-50ms
+#define ISR_T1_INTERVAL 46080 //0.99998911s as simulation result.  //922-1ms,1843-2ms,4608-5ms,9216-10ms,46080-50ms
 
 unsigned char taskCycle_10ms=0;
 unsigned char taskCycle_100ms=0;
@@ -34,10 +34,10 @@ void tsk_init(void)
 	//初始化计时器中断
 	TMOD=0x11;
 	//T0
-	TH0=(65536-ISR_T0_INTERVAL)/256;//晶振频率为11.014357Mhz，机器周期为12/11014357=1.089487112139183us,计1ms需要1000/1.089=918
+	TH0=(65536-ISR_T0_INTERVAL)/256;//晶振频率为11.0592Mhz，机器周期为12/11059200=1.085069444444444us,计1ms需要1000/1.085=922
 	TL0=(65536-ISR_T0_INTERVAL)%256;
     //T1
-	TH1=(65536-ISR_T1_INTERVAL)/256;//晶振频率为11.014357Mhz，机器周期为12/11014357=1.089487112139183us,计1ms需要1000/1.089=918
+	TH1=(65536-ISR_T1_INTERVAL)/256;//晶振频率为11.0592Mhz，机器周期为12/11059200=1.085069444444444us,计1ms需要1000/1.085=922
 	TL1=(65536-ISR_T1_INTERVAL)%256;
 	EA=1;
 	ET0=1;
